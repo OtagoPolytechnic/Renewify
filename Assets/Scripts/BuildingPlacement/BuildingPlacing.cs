aa6a2ffc32991eb14d67fce6ef353afc4e82e6b9
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2024-03-11 14:07:26 +1300

    Added basic code structure for building placement

--- Assets/Scripts/BuildingPlacement/BuildingPlacing.cs
+++ Assets/Scripts/BuildingPlacement/BuildingPlacing.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PanelPlacing : MonoBehaviour
+public class BuildingPlacing : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,39 @@ public class PanelPlacing : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        //If the cursor is over one of the building selection panels
+        selectBuilding();
+        //If there is a building selected and the mouse is over a grid space
+        placeBuilding();
+    }
+
+    selectBuilding()
+    {
+        //Get the building that was clicked on
+        //Set the building to the selected building
+    }
+
+    private void placeBuilding()
+    {
+        //Get X, Y, and Size from the GridCreator script
+        int playerX = 2;
+        int playerY = 2;
+        int gridSize = 5;
+        index = playerX * gridSize + playerY;
+        List<GameObject> tiles = new List<GameObject>();
+        //Check if the space is empty
+        if (tiles[index].empty == true)
+        {
+            //Set space to full
+            tiles[index].empty = false;
+            //Place the building
+        }
+        {
+            //Show a message saying that the space is full
+        }
     }
 }
