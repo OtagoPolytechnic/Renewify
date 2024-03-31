9c86a14b2a1562f312047e2622df031016c9bdb6
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2024-04-01 12:00:26 +1300

    Added temporary GOAL tiletype for testing

--- Assets/Scripts/TileTypes.cs
+++ Assets/Scripts/TileTypes.cs
@@ -7,7 +7,8 @@ public enum TileTypes
     None,
     SolarPanels,
     Windmills,
-    Trees, 
+    Trees,
     Rocks,
-    Wires
+    Wires, //This is for both straight and corner wires as outside of the placement script there is no need to know the difference
+    Goal //NOTE: THIS IS HERE FOR TESTING THE WIRE PLACEMENT BRANCH AS I DON'T HAVE A GOAL TILE YET
 }
