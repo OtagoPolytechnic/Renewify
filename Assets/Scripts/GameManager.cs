4dbc23a53a06cd53684e5c270417de963baf8e8f
unknown <mckeld3@student.op.ac.nz>
2024-04-02 19:37:13 +1300

    ADD: update function to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public int totalScore = 0;
     // Start is called before the first frame update
     void Awake()
     {
@@ -13,20 +14,25 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
     }
+    void Update()
+    {
+        //TODO: This function doesn't need to be called every frame. It could be called when placing a building potentially?
+        CalculateTotalScore(GridManager.Instance.tileStates, GridManager.Instance.tileBonus);
+    }
     /// <summary>
     /// 
     /// </summary>
     /// <param name="tileStates"></param>
     /// <param name="tileBonus"></param>
     /// <returns></returns>
-    
-    /// Generated with help from ChatGPT using following prompt:
-    /// I want to constantly calculate total score of a grid. I have a list of grid states, and a list of bools, these lists map to the grid.
-    /// so I want to check if the gridstates[i] has a building and if boolList[i] is true 
-    /// unity c#
     public int CalculateTotalScore(List<TileTypes> tileStates, List<bool> tileBonus)
     {
-        int totalScore = 0;
+        /// Generated with help from ChatGPT using following prompt:
+        /// I want to constantly calculate total score of a grid. I have a list of grid states, and a list of bools, these lists map to the grid.
+        /// so I want to check if the gridstates[i] has a building and if boolList[i] is true 
+        /// unity c#
+
+        totalScore = 0; //reset score before changing it in loop below
         for(int i = 0; i < tileStates.Count; i++)
         {
             if(tileStates[i] == TileTypes.Windmills && tileBonus[i])
