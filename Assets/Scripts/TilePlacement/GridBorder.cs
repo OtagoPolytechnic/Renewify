7def1e9be3165854c036d2ec53577cd26f8790f6
unknown <mckeld3@student.op.ac.nz>
2024-04-08 09:49:40 +1200

    ADD: gridborder script

---
+++ Assets/Scripts/TilePlacement/GridBorder.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script will spawn in and resizing the grid border
+/// </summary>
+public class GridBorder : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void SpawnBorder()
+    {
+
+    }
+}
