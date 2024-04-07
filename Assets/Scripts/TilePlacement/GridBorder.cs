dc64132f943f8a66c13e93973958846239c28ab7
unknown <mckeld3@student.op.ac.nz>
2024-04-08 10:11:38 +1200

    border position now changes based on gridsize

--- Assets/Scripts/TilePlacement/GridBorder.cs
+++ Assets/Scripts/TilePlacement/GridBorder.cs
@@ -10,25 +10,30 @@ using UnityEngine;
 public class GridBorder : MonoBehaviour
 {
     public GameObject borderPrefab; //assign this in inspector
+    public int positionOffset;
     void Start()
     {
-        SpawnBorder();
+        positionOffset = GridManager.Instance.gridSize * 5; //if grid size is 10, the borders should be at 50
+        
+        Vector3[] spawnPositions = new Vector3[]
+        {
+            new Vector3(-positionOffset, 1, 0),
+            new Vector3(positionOffset, 1, 0), 
+            new Vector3(0, 1, -positionOffset),
+            new Vector3(0, 1, positionOffset) 
+        };
+        
+        SpawnBorder(spawnPositions);
     }
 
-    Vector3[] spawnPositions = new Vector3[]
-    {
-        new Vector3(-50, 1, 0),
-        new Vector3(50, 1, 0), 
-        new Vector3(0, 1, -50),
-        new Vector3(0, 1, 50) 
-    };
+    
 
-    public void SpawnBorder()
+    public void SpawnBorder(Vector3[] spawnPositions)
     {
         for (int i = 0; i < 4; i++)
         {
             Quaternion spawnRotation;
-            
+
             if (spawnPositions[i].x != 0)
             {
                 spawnRotation = Quaternion.Euler(0, 90, 0);
