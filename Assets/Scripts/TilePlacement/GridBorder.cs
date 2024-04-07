47d8123883abf9d06350ce97415e6277af085dee
unknown <mckeld3@student.op.ac.nz>
2024-04-08 09:57:17 +1200

    ADD: function to instantiate borders

--- Assets/Scripts/TilePlacement/GridBorder.cs
+++ Assets/Scripts/TilePlacement/GridBorder.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// This script will spawn in and resizing the grid border
+/// This script will spawn in and resizing the grid border.
+/// 
+/// This script should be added to an empty gameobject at position 0,0,0
 /// </summary>
 public class GridBorder : MonoBehaviour
 {
-    
+    public GameObject borderPrefab; //assign this in inspector
     void Start()
     {
-        
+        SpawnBorder();
     }
 
     public void SpawnBorder()
     {
-
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject border = Instantiate(borderPrefab, new Vector3(0,1,0), Quaternion.identity);
+            border.name = "Border_" + i;
+            //GridManager.Instance.gridSize;
+        }
     }
 }
