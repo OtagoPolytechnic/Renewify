40c9816ac59f502b6c0883f01a9b6a2d76727bff
Carlson-poly <carlsonzhong@gmail.com>
2024-04-11 14:55:03 +1200

    Animation script

--- Assets/Scripts/BuildingPlacement/AnimationTrigger.cs
+++ Assets/Scripts/BuildingPlacement/AnimationTrigger.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class AnimationTrigger : MonoBehaviour
 {
-    public GameObject windmillPrefab;
     private Animator windmillAnimator;
-
+    private Vector2 newVector2;
 
     // Start is called before the first frame update
     void Start()
     {
-        windmillAnimator = windmillPrefab.GetComponent<Animator>();
+        windmillAnimator = gameObject.GetComponent<Animator>();
+
+        //get the parent of gameObject
+        Transform parent = gameObject.transform.parent;
+        if(parent != null)
+        {
+            char x = parent.gameObject.name[5];
+            char y = parent.gameObject.name[7];
+            newVector2 = new Vector2(x,y);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //bool isTileConnected = WirePlacement.Instance.isTileConnected(1);
+        //Implement after vector2 refactoring
+        //bool isTileConnected = WirePlacement.Instance.isTileConnected(newVector2);
 
-        windmillAnimator.SetBool("connected". isTileConnected);
+        //Testing for current codebase
+        bool isTileConnected = WirePlacement.Instance.isTileConnected(1);
+
+        windmillAnimator.SetBool("Connected", isTileConnected);
     }
 
-    
 }
