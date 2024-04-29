73fcf60242c63e90e03ea7c5d38ac39f3443c43d
Carlson-poly <carlsonzhong@gmail.com>
2024-04-29 14:20:46 +1200

    Change code into vector2

--- Assets/Scripts/BuildingPlacement/AnimationTrigger.cs
+++ Assets/Scripts/BuildingPlacement/AnimationTrigger.cs
@@ -18,19 +18,21 @@ public class AnimationTrigger : MonoBehaviour
         {
             char x = parent.gameObject.name[5];
             char y = parent.gameObject.name[7];
-            newVector2 = new Vector2(x,y);
+            
+            int x_ = int.Parse(x.ToString());
+            int y_ = int.Parse(y.ToString());
+            newVector2 = new Vector2(x_,y_);
         }
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Implement after vector2 refactoring
-        //bool isTileConnected = WirePlacement.Instance.isTileConnected(newVector2);
-
-        //Testing for current codebase
-        bool isTileConnected = WirePlacement.Instance.isTileConnected(1);
+        //check if tile is connected to a building
+        bool isTileConnected = WirePlacement.Instance.isTileConnected(GridManager.GetTileIndex(newVector2));
+        //Debug.Log(GridManager.GetTileIndex(newVector2));
+        
 
         windmillAnimator.SetBool("Connected", isTileConnected);
     }
