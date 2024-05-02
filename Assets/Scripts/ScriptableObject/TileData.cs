246841556c65884a52111e2bb6893130ee202368
Liam McKenzie (1000103822) <MCKELD3@student.op.ac.nz>
2024-05-02 15:07:56 +1200

    ADD: scriptable object

---
+++ Assets/Scripts/ScriptableObject/TileData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : ScriptableObject
+{
+    public int levelIndex;
+    
+
+    /*
+    data to load:
+    list of spots worth points.
+    - type of power
+    - manually enter main tile
+    - calculate 8 nearby tiles at runtime
+    - specify tile position
+
+    */
+}
