2cae4d0ae6aa17ba564f93735c40dd416b2e0217
Liam McKenzie (1000103822) <MCKELD3@student.op.ac.nz>
2024-05-09 14:27:41 +1200

    ADD: pause shortcut

--- Assets/Scripts/Menu/PauseGame.cs
+++ Assets/Scripts/Menu/PauseGame.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 /// <summary>
 /// This script controls pausing the game. This will likely be used for the pause menu
-/// 
-/// WIP: This script is currently unused.
 /// </summary>
 public class PauseGame : MonoBehaviour
 {
@@ -13,13 +11,26 @@ public class PauseGame : MonoBehaviour
     //Some code will still run while paused, the CheckMouseHover() function in MouseManager.cs is a crutial function that will still run when paused. 
     //So this function now first checks if the game is unpaused before running 
     //Source: https://www.youtube.com/watch?v=ROwsdftEGF0 
+
+    public static PauseGame Instance;
+    public bool isPaused = false;
+    void Start()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }   
+
     public void Pause()
     {
+        isPaused = true;
         Time.timeScale = 0;
     }
 
     public void UnPause() //Make sure to call this function when exiting the main scene from the pause menu
     {
+        isPaused = false;
         Time.timeScale = 1;
     }
 }
