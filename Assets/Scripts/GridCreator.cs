211fe7bba8c70df04c4ce2612bc8b909ad956309
Palin Wiseman <WISEPB1@student.op.ac.nz>
2024-05-13 14:50:26 +1200

    Added tile colouring based on point generation

--- Assets/Scripts/GridCreator.cs
+++ Assets/Scripts/GridCreator.cs
@@ -12,6 +12,12 @@ public class GridCreator : MonoBehaviour
     public Material debugMaterial;
 
     public static List<GameObject> tiles = new List<GameObject>();
+    public Material Wind1;
+    public Material Wind2;
+    public Material Wind3;
+    public Material Sun1;
+    public Material Sun2;
+    public Material Sun3;
     
     void Awake()
     {
@@ -29,7 +35,7 @@ public class GridCreator : MonoBehaviour
             
             if(GridManager.Instance.tileBonus[i] == true)
             {
-                DebugTileScore(i);
+                AddBonusTile(GridManager.GetTilePosition(i), false);
             }
         }
     }
@@ -63,5 +69,83 @@ public class GridCreator : MonoBehaviour
 
         tile.GetComponent<Renderer>().material = debugMaterial;
     }
+    private void AddBonusTile(Vector2 centerTile, bool wind)
+    {
+        int index = GridManager.GetTileIndex(centerTile);
+        List<Vector2> tiles2 = new List<Vector2>()
+        {
+            new Vector2(centerTile.x + 1, centerTile.y),
+            new Vector2(centerTile.x - 1, centerTile.y),
+            new Vector2(centerTile.x, centerTile.y + 1),
+            new Vector2(centerTile.x, centerTile.y - 1)
+        };
+        List<Vector2> tiles3 = new List<Vector2>()
+        {
+            new Vector2(centerTile.x + 1, centerTile.y + 1),
+            new Vector2(centerTile.x - 1, centerTile.y - 1),
+            new Vector2(centerTile.x + 1, centerTile.y - 1),
+            new Vector2(centerTile.x - 1, centerTile.y + 1)
+        };
+        if(wind)
+        {
+            if(ValidCheck(centerTile))
+            {
+                BonusTileCreate(index, Wind1);
+            }
+            foreach(Vector2 v in tiles2)
+            {
+                if(ValidCheck(v))
+                {
+                    BonusTileCreate(GridManager.GetTileIndex(v), Wind2);
+                }
+            }
+            foreach(Vector2 v in tiles3)
+            {
+                if(ValidCheck(v))
+                {
+                    BonusTileCreate(GridManager.GetTileIndex(v), Wind3);
+                }
+            }
+        }
+        else
+        {
+            if(ValidCheck(centerTile))
+            {
+                BonusTileCreate(index, Sun1);
+            }
+            foreach(Vector2 v in tiles2)
+            {
+                if(ValidCheck(v))
+                {
+                    BonusTileCreate(GridManager.GetTileIndex(v), Sun2);
+                }
+            }
+            foreach(Vector2 v in tiles3)
+            {
+                if(ValidCheck(v))
+                {
+                    BonusTileCreate(GridManager.GetTileIndex(v), Sun3);
+                }
+            }
+        }
+    }
 
+    private void BonusTileCreate(int index, Material material)
+    {
+        Vector3 tilePosition = tiles[index].transform.position;
+        GameObject tile = Instantiate(tilePrefab, tilePosition, Quaternion.identity);
+        tile.name = "Bonus_Tile_" + index;
+        tile.tag = "Untagged";
+        tile.GetComponent<MeshCollider>().enabled = false;
+        tile.GetComponent<Renderer>().material = material;
+    }
+
+    private bool ValidCheck(Vector2 v)
+    {
+        if(v.x < 0 || v.x >= GridManager.Instance.gridSize || v.y < 0 || v.y >= GridManager.Instance.gridSize || !GridManager.IsTileEmpty(GridManager.GetTileIndex(v)))
+        {
+            return false; //This is to prevent the game from looping the tiles around the map or placing underneath an obstacle
+        }
+        return true;
+    }
 }
